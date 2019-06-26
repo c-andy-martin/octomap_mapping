4791a0f890c6eeb3c934818508f8a15c2051e84f
C. Andy Martin <cam@badger-technologies.com>
2019-06-26 09:43:02 -0400

    Move OcTreeT, pcl types into separate header
    Move the OcTreeT and PCLPointCloud types into a separate header.
    They need to be used by other classes. To avoid a circular header
    dependency, split these types out into their own header.

---
+++ octomap_server/include/octomap_server/types.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2010-2013, A. Hornung, University of Freiburg
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the University of Freiburg nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef OCTOMAP_SERVER_TYPES_H
+#define OCTOMAP_SERVER_TYPES_H
+
+#include <pcl/point_types.h>
+#include <pcl/filters/passthrough.h>
+#include <octomap_server/OcTreeStampedWithExpiry.h>
+
+namespace octomap_server {
+
+#ifdef COLOR_OCTOMAP_SERVER
+typedef pcl::PointXYZRGB PCLPoint;
+typedef pcl::PointCloud<pcl::PointXYZRGB> PCLPointCloud;
+typedef octomap::ColorOcTree OcTreeT;
+#else
+typedef pcl::PointXYZ PCLPoint;
+typedef pcl::PointCloud<pcl::PointXYZ> PCLPointCloud;
+typedef octomap_server::OcTreeStampedWithExpiry OcTreeT;
+#endif
+
+}  // end namespace octomap_server
+
+#endif  // OCTOMAP_SERVER_TYPES_H
